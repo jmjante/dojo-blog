b8963aac5f24ec8aac6690f3527acd38fdd2ad5c
jmjante <jmjante@gmail.com>
2022-09-01 10:59:54 +0800

    added click event on home component, added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
-  const title = 'hallooooo'
+  const titleHomePage = 'HomePage'
   return (
     <div className="App">
       <Navbar />
-      <div className="content"></div>
-        <Home title = {title}/>
+      <div className="content">
+        <Home title = {titleHomePage}/>
+      </div>
+
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 const Home = ({title}) => {
+
+  const handleClick = (name, e) => {
+    console.log(`hello ${name}`, e.target)
+    // console.log(e)
+  }
+
   return (
-    <h1>
-        {title}
-    </h1>
+    <div className="home">
+      <h2>
+          {title}
+      </h2>
+      <button onClick={(e) => handleClick('JM', e)}>Click Me</button>
+    </div>
+
   )
 }
 export default Home
